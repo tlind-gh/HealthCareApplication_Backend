20f0174c7e6e156cb7b9d08b4dfca9d7b39dc3f4
falk-adam <falk47.adam@gmail.com>
2026-01-14 15:20:04 +0100

    gave Appointment model basic fields with geter and seters

---
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/Appointment.java
@@ -0,0 +1,79 @@
+package healthcareab.project.healthcare_booking_app.models;
+
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Document(collection="appointments")
+public class Appointment {
+    
+    @Id
+    private String id;
+    
+    @NotNull
+    private String providerId;
+    
+    @NotNull
+    private LocalDate date;
+    
+    @NotNull
+    private LocalTime startTime;
+    
+    @NotNull
+    private LocalTime endTime;
+    
+    private String status;
+    
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getProviderId() {
+        return providerId;
+    }
+    
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+    
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+    
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
