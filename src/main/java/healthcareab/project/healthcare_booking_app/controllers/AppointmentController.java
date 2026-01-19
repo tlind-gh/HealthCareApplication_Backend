4dff400a5b3ee0d93e4c9a285e523178f2b383a6
Teo Lind <teo.l.lind@gmail.com>
2026-01-19 20:31:26 +0100

    added rudimentary endpoints for get and patch

--- src/main/java/healthcareab/project/healthcare_booking_app/controllers/AppointmentController.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/controllers/AppointmentController.java
@@ -1,18 +1,15 @@
 package healthcareab.project.healthcare_booking_app.controllers;
 
 import healthcareab.project.healthcare_booking_app.dto.AppointmentRequest;
-
 import healthcareab.project.healthcare_booking_app.dto.AppointmentResponse;
-
 import healthcareab.project.healthcare_booking_app.services.AppointmentService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/appointment")
@@ -27,5 +24,26 @@ public class AppointmentController {
     public ResponseEntity<AppointmentResponse> createAppointment(@Valid @RequestBody AppointmentRequest request) {
         AppointmentResponse response = appointmentService.createAppointment(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentResponse>> getAppointmentsCurrentUser() {
+        return new ResponseEntity<>(appointmentService.getAppointmentsCurrentUser(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppointmentResponse> getAppointmentById(@PathVariable String id) {
+        return new ResponseEntity<>(appointmentService.getAppointmentById(id), HttpStatus.OK);
+    }
+
+    @PatchMapping("/cancel/{id}/")
+    public ResponseEntity<AppointmentResponse> cancelAppointment(@PathVariable String id) {
+        return new ResponseEntity<>(appointmentService.cancelAppointment(id), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/all")
+    public ResponseEntity<List<AppointmentResponse>> getAllAppointments() {
+        return new ResponseEntity<>(appointmentService.getAllAppointments(), HttpStatus.OK);
     }
 }
--- src/main/java/healthcareab/project/healthcare_booking_app/services/AppointmentService.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/AppointmentService.java
@@ -9,11 +9,12 @@ import healthcareab.project.healthcare_booking_app.models.User;
 import healthcareab.project.healthcare_booking_app.models.supportClasses.AppointmentStatus;
 import healthcareab.project.healthcare_booking_app.models.supportClasses.Role;
 import healthcareab.project.healthcare_booking_app.repositories.AppointmentRepository;
+import healthcareab.project.healthcare_booking_app.repositories.UserAuthRepository;
 import healthcareab.project.healthcare_booking_app.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AppointmentService {
@@ -21,17 +22,19 @@ public class AppointmentService {
     private final AvailabilityService availabilityService;
     private final UserService userService;
     private final UserRepository userRepository;
-    
+    private final UserAuthRepository userAuthRepository;
+
     public AppointmentService(
             AppointmentRepository appointmentRepository,
             AvailabilityService availabilityService,
             UserService userService,
-            UserRepository userRepository
-    ) {
+            UserRepository userRepository,
+            UserAuthRepository userAuthRepository) {
         this.appointmentRepository = appointmentRepository;
         this.availabilityService = availabilityService;
         this.userService = userService;
         this.userRepository = userRepository;
+        this.userAuthRepository = userAuthRepository;
     }
     
     public AppointmentResponse createAppointment(AppointmentRequest request) {
@@ -77,6 +80,54 @@ public class AppointmentService {
         Appointment savedAppointment = appointmentRepository.save(appointment);
         
         return mapToResponse(savedAppointment);
+    }
+
+    public List<AppointmentResponse> getAppointmentsCurrentUser() {
+        User currentUser = userAuthRepository.authenticateAndExtractUser();
+        List<Appointment> appointments = new ArrayList<>();
+        if (currentUser.getRoles().contains(Role.PATIENT)) {
+            appointments = appointmentRepository.findByPatientId(currentUser.getId());
+        } else if (currentUser.getRoles().contains(Role.PROVIDER)) {
+            appointments = appointmentRepository.findByProviderId(currentUser.getId());
+        }
+        return appointments.stream().map(this::mapToResponse).toList();
+    }
+
+    public AppointmentResponse getAppointmentById(String id) {
+        Appointment appointment = getAppointmentFromRepository(id);
+        authorizeUserAccessToAppointment(appointment);
+        return mapToResponse(appointment);
+    }
+
+    public AppointmentResponse cancelAppointment(String id) {
+        Appointment appointment = getAppointmentFromRepository(id);
+        authorizeUserAccessToAppointment(appointment);
+        if (appointment.getStatus().equals(AppointmentStatus.CANCELLED)) {
+            throw new UnsupportedOperationException("Appointment has already been cancelled");
+        }
+        //TODO: add check for last cancellation time
+        appointment.setStatus(AppointmentStatus.CANCELLED);
+        return mapToResponse(appointmentRepository.save(appointment));
+    }
+
+    public List<AppointmentResponse> getAllAppointments() {
+        if (!userAuthRepository.authenticateAndExtractUser().getRoles().contains(Role.ADMIN)) {
+            throw new UnauthorizedException("User must be admin to see all appointments");
+        }
+        return appointmentRepository.findAll().stream().map(this::mapToResponse).toList();
+    }
+
+    private Appointment getAppointmentFromRepository(String id) {
+        return appointmentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Appointment Not Found"));
+    }
+
+    private void authorizeUserAccessToAppointment(Appointment appointment) {
+        User currentUser = userAuthRepository.authenticateAndExtractUser();
+        if (!currentUser.getId().equals(appointment.getPatientId()) && !currentUser.getId().equals(appointment.getProviderId())
+                && !currentUser.getRoles().contains(Role.ADMIN)) {
+            throw new UnauthorizedException("User is not the patient or provider for the appointment, nor admin");
+        }
     }
     
     private AppointmentResponse mapToResponse(Appointment appointment) {
